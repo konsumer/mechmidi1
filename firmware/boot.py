c82260f46469bcb6715808489ebaf331872b2f03
David Konsumer <konsumer@jetboystudio.com>
2025-03-05 04:29:08 -0800

    sam/midi demos

--- firmware/boot.py
+++ firmware/boot.py
@@ -1,6 +1,14 @@
 import supervisor
+import usb_hid
+import usb_midi 
 
 # disable writing REPL to OLED (not useful and ugly)
+# it will still turn on if you break the loop
+# this is just for initial boot
 supervisor.status_bar.display = False
 supervisor.status_bar.console = False
 display.root_group = None
+
+# disable acting as HID device, enable USB MIDI
+usb_hid.disable()
+usb_midi.enable()
--- firmware/code.py
+++ firmware/code.py
@@ -9,14 +9,28 @@ import displayio
 import terminalio
 from adafruit_display_text import label
 from i2cdisplaybus import I2CDisplayBus
-
 from adafruit_led_animation.color import RAINBOW, RED, YELLOW, ORANGE, GREEN, TEAL, CYAN, BLUE, PURPLE, MAGENTA, WHITE, BLACK, GOLD, PINK, AQUA, JADE, AMBER, OLD_LACE
 from adafruit_led_animation.animation.rainbowcomet import RainbowComet
 from adafruit_led_animation.animation.sparklepulse import SparklePulse
-
 import adafruit_displayio_ssd1306
+import adafruit_midi
+import usb_midi
+from adafruit_midi.note_on import NoteOn
+from adafruit_midi.note_off import NoteOff
+from adafruit_midi.control_change import ControlChange
+from adafruit_midi.program_change import ProgramChange
+from adafruit_midi.system_exclusive import SystemExclusive
+import saminfo
+from mechmidi1 import Menu,ROT0S,ROT0A,ROT0B,ROT1S,ROT1A,ROT1B,SDA,SCL,LED,ROW1,ROW2,ROW3,ROW4,ROW5,COL1,COL2,COL3,COL4,TX
 
-from mechmidi1 import Menu,ROT0S,ROT0A,ROT0B,ROT1S,ROT1A,ROT1B,SDA,SCL,LED,ROW1,ROW2,ROW3,ROW4,ROW5,COL1,COL2,COL3,COL4
+# setup USB midi on all channels
+midi = adafruit_midi.MIDI(midi_in=usb_midi.ports[0] )
+
+# this is for SAM2695 on TX
+uart = busio.UART(tx=TX, baudrate=31250, bits=8, parity=None, stop=1)
+sam = adafruit_midi.MIDI(midi_out=uart, out_channel=0)
+sam.send(SystemExclusive([0x7e, 0x7f], [0x09, 0x01])) # Reset
+sam.send(SystemExclusive([0x7f, 0x7f, 0x04], [0x01, 0x00, 123 & 0x7f])) # Volume 123
 
 # setup i2c and OLED
 displayio.release_displays()
@@ -76,6 +90,109 @@ screen.append(t)
 
 ###
 
+
+# OLED makes SAM a bit delayed, so I don't update screen while it's running
+def demo_midisam(t):
+  # you can map patches to channels like this
+  sam.send(ProgramChange(patch=0), channel=0) # Piano
+  sam.send(ProgramChange(patch=17), channel=1) # Drawbar Organ
+
+  t.text = "Send USB MIDI\n"
+  while True:
+    m = midi.receive()
+    if m:
+      # forward USB MID to same and show stuff on screen
+      if isinstance(m, NoteOff):
+        sam.send(NoteOff(note=m.note), channel=m.channel)
+      if isinstance(m, NoteOn):
+        sam.send(NoteOn(note=m.note), channel=m.channel)
+      if isinstance(m, ControlChange):
+        sam.send(ControlChange(control=m.control, value=m.value), channel=m.channel)
+      if isinstance(m, ProgramChange):
+        sam.send(ProgramChange(patch=m.patch), channel=m.channel)
+    if not rotb[0].value and not rotb[1].value:
+      break
+  sleep(0.2)
+
+# Just show MIDI stuff on screen
+def demo_midi(t):
+  t.text = "Send USB MIDI\n"
+  while True:
+    m = midi.receive()
+    if m:
+      # forward USB MID to same and show stuff on screen
+      if isinstance(m, NoteOff):
+        t.text = f"MIDI\nnote:{m.note}\nvelocity:{m.velocity}\nchannel:{m.channel}"
+      if isinstance(m, NoteOn):
+        t.text = f"MIDI\nnote:{m.note}\nvelocity:{m.velocity}\nchannel:{m.channel}"
+      if isinstance(m, ControlChange):
+        t.text = f"MIDI\ncontrol:{m.control}\nvalue:{m.value}\nchannel:{m.channel}"
+      if isinstance(m, ProgramChange):
+        t.text = f"MIDI\nprogram:{m.patch}\nchannel:{m.channel}"
+    if not rotb[0].value and not rotb[1].value:
+      break
+  sleep(0.2)
+
+# just play piano on SAM
+# notes start at C3
+def demo_piano(t):
+  pixels.fill(BLACK)
+  pixels.show()
+  inst = rot[0].position % 128
+  oldval = inst
+  t.text = f"({inst+1}) {saminfo.patches[inst]}\nPress keys to play\nPress both rotaries\nto exit\n"
+  sam.send(ProgramChange(patch=inst), channel=0) # set proagram to rot position
+  while True:
+    inst = rot[0].position % 128
+    if oldval != inst:
+      t.text = f"({inst+1}) {saminfo.patches[inst]}\nPress keys to play\nPress both rotaries\nto exit\n"
+      sam.send(ProgramChange(patch=inst), channel=0) # set proagram to rot position
+      oldval = inst
+    e = keys.events.get()
+    if e:
+      if e.pressed:
+        pixels[e.key_number] = RED
+        sam.send(NoteOn(note=e.key_number+48, velocity=100), channel=0)
+        pixels.show()
+      else:
+        pixels[e.key_number] = BLACK
+        sam.send(NoteOff(note=e.key_number+48), channel=0)
+        pixels.show()
+    if not rotb[0].value and not rotb[1].value:
+      break
+  pixels.fill(BLACK)
+  pixels.show()
+  sleep(0.2)
+
+# just play drums on SAM
+def demo_drums(t):
+  pixels.fill(BLACK)
+  pixels.show()
+  inst = rot[0].position % 5
+  oldval = inst
+  t.text = f"{saminfo.drums[inst]}\nPress keys to play\nPress both rotaries\nto exit\n"
+  sam.send(ProgramChange(patch=saminfo.drum_programs[inst]), channel=9) # set proagram to rot position
+  while True:
+    inst = rot[0].position % 5
+    if oldval != inst:
+      t.text = f"{saminfo.drums[inst]}\nPress keys to play\nPress both rotaries\nto exit\n"
+      sam.send(ProgramChange(patch=saminfo.drum_programs[inst]), channel=9) # set proagram to rot position
+      oldval = inst
+    e = keys.events.get()
+    if e:
+      if e.pressed:
+        pixels[e.key_number] = GREEN
+        sam.send(NoteOn(note=e.key_number+36, velocity=100), channel=9)
+        pixels.show()
+      else:
+        pixels[e.key_number] = BLACK
+        sam.send(NoteOff(note=e.key_number+36), channel=9)
+        pixels.show()
+    if not rotb[0].value and not rotb[1].value:
+      break
+  pixels.fill(BLACK)
+  pixels.show()
+  sleep(0.2)
 
 def demo_input():
   while True:
@@ -163,7 +280,7 @@ class MenuLED(Menu):
 
 class MenuDemos(Menu):
   def __init__(self, textArea, rot, button):
-    super(MenuDemos, self).__init__("Demos", ["LEDs", "Input", "Logo"], textArea, rot, button)
+    super(MenuDemos, self).__init__("Demos", ["LEDs", "Input", "Logo", "MIDI SAM2695", "MIDI Debug", "Piano", "Drums"], textArea, rot, button)
 
   def select(self, position):
     global currentMode
@@ -176,9 +293,16 @@ class MenuDemos(Menu):
     if position == 2:
       self.t.text = ""
       logo_display()
+    if position == 3:
+      demo_midisam(t)
+    if position == 4:
+      demo_midi(t)
+    if position == 5:
+      demo_piano(t)
+    if position == 6:
+      demo_drums(t)
 
 currentMode = MenuDemos(t, rot[0], rotb[0])
 
 while True:
-  keys.events.get()
   currentMode.show()
